357038dc5cb5a60c68fb71307cec23b4de9cec1a
Paulo Cabreira <paulocabreira@gmail.com>
2022-07-16 21:15:23 -0300

    Mock de herois + Material (toolbar) + Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
