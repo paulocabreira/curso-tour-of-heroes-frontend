c6d2a0a42c46723ec17ccea1f39eee52177f7e98
Paulo Cabreira <paulocabreira@gmail.com>
2022-11-02 16:34:43 -0300

    Adicionando Navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -25,8 +30,11 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
     MatExpansionModule,
     AppRoutingModule
   ],
