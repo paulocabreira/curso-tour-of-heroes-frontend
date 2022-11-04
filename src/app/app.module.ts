2fb65ad3913f74d9703e2c471303fc8985c3409f
Paulo Cabreira <paulocabreira@gmail.com>
2022-11-04 19:36:46 -0300

    Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,22 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MaterialModule } from './material/material.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
-    MaterialModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
