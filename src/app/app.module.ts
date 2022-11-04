78bfb252b32c6ff5e1416ca9919f7a2f5ef71696
Paulo Cabreira <paulocabreira@gmail.com>
2022-11-04 20:07:20 -0300

    Feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    // @angular
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+
+    // feature
+    HeroesModule,
+    DashboardModule,
+
+    // app
     CoreModule,
     AppRoutingModule
   ],
