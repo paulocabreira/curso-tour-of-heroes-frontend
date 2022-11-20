1d6be61d88ce9b5d4c83c6258246a4dab098cc9c
Paulo Cabreira <paulocabreira@gmail.com>
2022-11-20 18:27:24 -0300

    Adicionando token

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/common/http';
 import { catchError, Observable, throwError} from 'rxjs';
 import { MessageService } from '../services/message.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -17,10 +18,21 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
+
+        if (!environment.production) {
+          console.log(err);
+        }
+
         let errMsg = '';
 
         if (err.error instanceof ErrorEvent) {
           errMsg = `Error: ${err.error.message}`;
+        }
+        else if (Array.isArray(err.error) && err.error.length) {
+          errMsg = `Error: ${err.error[0]}`;
+        }
+        else if (err.error.errors) {
+          errMsg = `Error: ${err.error.errors}`
         }
         else {
           errMsg = `Error Code: ${err.status}, Message: ${err.message}`;
